2950198be8211307b67b19d11da7e785e8049ae0
Jonathan Pagel <pagel@pobox.com>
2012-08-10 17:26:28 +0100

    put in db credentials for app server

--- public/db.inc.php
+++ public/db.inc.php
@@ -1,10 +1,10 @@
 <?php
 function getDbcredentials(){
 	return array(
-		'dbname' => 'quipper',
-		'dbuser' => 'quipper',
-		'dbpass' => 'reppiuq',
-		'dbhost' => 'localhost'
+		'dbname' => 'db_792fa253',
+		'dbuser' => 'user_792fa253',
+		'dbpass' => 'zIa^ROE3yIElF8',
+		'dbhost' => 'a.db.shared.orchestra.io'
 	);
 }
 
