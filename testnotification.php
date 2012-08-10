a90c7e11b5da11639783665461428fe09a6f7b75
Jonathan Pagel <pagel@pobox.com>
2012-08-10 16:17:20 +0100

    removed redundant files

--- testnotification.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('db.inc.php');
-include('lib.inc.php');
-
-$message = "You've been invited to a game!!!!!";
-
-sendIosNotification('178893bc87866f0f44d5d7bb4c3e5b84c0c3fa31b45e7e397a683f820919d89e', $message);
