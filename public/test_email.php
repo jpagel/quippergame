f92d331841913fe8286de9f90f79434ba4b0ef41
Jonathan Pagel <pagel@pobox.com>
2012-08-13 11:31:49 +0100

    test email script

---
+++ public/test_email.php
@@ -0,0 +1,12 @@
+<?php
+var_dump($testemail());
+
+function testemail(){
+    $to = 'jfpagel@gmail.com';
+    $subject = 'test email from quippergame';
+    $body = date( 'Ymd H:i:s' ) . "\n\nJust testing.\nLove, the quippergame API";
+    $headers = "From: api@quipper.com\r\n" .
+                "Reply-To: jfpagel@fasstmail.fm\r\n" .
+                "X-Mailer: PHP/' . phpversion();
+    return mail( $to, $subject, $body, $headers );
+}
