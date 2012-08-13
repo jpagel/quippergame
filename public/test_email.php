9c5c748c24d9fec96410f631420ecb54f40768ab
Jonathan Pagel <pagel@pobox.com>
2012-08-13 16:52:14 +0100

    adapted test to curl gridmail

--- public/test_email.php
+++ public/test_email.php
@@ -1,5 +1,5 @@
 <?php
-var_dump(testemail());
+var_dump(gridmail());
 
 function testemail(){
     $to = 'jfpagel@gmail.com';
@@ -9,4 +9,21 @@ function testemail(){
                 "Reply-To: jfpagel@fasstmail.fm\r\n" .
                 "X-Mailer: PHP/" . phpversion();
     return mail( $to, $subject, $body, $headers );
+}
+
+function gridmail(){
+    
+    $to = 'jfpagel@gmail.com';
+    $toname = 'jojoiii';
+    $subject = 'test email from quippergame';
+    $body = date( 'Ymd H:i:s' ) . "\n\nJust testing.\nLove, the quippergame API";
+    $headers = "From: api@quipper.com\r\n" .
+                "Reply-To: jfpagel@fasstmail.fm\r\n" .
+                "X-Mailer: PHP/" . phpversion();
+    
+    $url = "https://sendgrid.com/api/mail.send.xml?api_user=jonathan@politicalcompass.org&api_key=secureSecret&to=$to&toname=$toname&subject=$subject&text=$body";
+    $ch = curl_init("http://www.example.com/");
+    $success = curl_exec( $ch );
+    fclose( $ch );
+    return $success;
 }
