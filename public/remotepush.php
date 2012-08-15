64b0048763e43afa1f1578247f7319c05b0d3826
Jonathan Pagel <pagel@pobox.com>
2012-08-15 14:11:09 +0100

    remote push script, for sending ios notifications without having to wait for the response

---
+++ public/remotepush.php
@@ -0,0 +1,56 @@
+<?php
+include( 'lib.inc.php' );
+
+$req = $_GET;
+
+$params = array(
+    'deviceid' => getArrayValue( $req, 'token' ),
+    'message' => trim( getArrayValue( $req, 'msg' ) )
+);
+
+if( ( $deviceid = $params[ 'deviceid' ] ) && ( $message = $params[ 'message' ] ) ){
+    var_dump( localSendIosNotification( $deviceid, $message ) );
+}
+
+function localSendIosNotification( $deviceToken, $message ){
+	$passphrase = 'quiz24';
+	
+	$ctx = stream_context_create();
+	stream_context_set_option($ctx, 'ssl', 'local_cert', 'ck.pem');
+	stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+	
+	// Open a connection to the APNS server
+	$fp = stream_socket_client(
+		'ssl://gateway.sandbox.push.apple.com:2195', $err,
+		$errstr, 60, STREAM_CLIENT_CONNECT|STREAM_CLIENT_PERSISTENT, $ctx);
+	
+	if (!$fp)
+		exit("Failed to connect: $err $errstr" . PHP_EOL);
+	
+	//echo 'Connected to APNS' . PHP_EOL;
+	
+	// Create the payload body
+	$body['aps'] = array(
+		'alert' => $message,
+		'sound' => 'default'
+		);
+	
+	// Encode the payload as JSON
+	$payload = json_encode($body);
+	
+	// Build the binary notification
+	$msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
+	
+	
+	// Send it to the server
+	$result = fwrite($fp, $msg, strlen($msg));
+	
+	if (!$result)
+		$status = 'Message not delivered' . PHP_EOL;
+	else
+		$status = 'Message successfully delivered' . PHP_EOL;
+	
+	// Close the connection to the server
+	fclose($fp);
+	return $status;
+}
