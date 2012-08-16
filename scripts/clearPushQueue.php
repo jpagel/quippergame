d5cdd03d0d2027b6d1801285338e99750e73cbd1
Jonathan Pagel <pagel@pobox.com>
2012-08-16 16:42:44 +0100

    faffing around for testing

--- scripts/clearPushQueue.php
+++ scripts/clearPushQueue.php
@@ -2,6 +2,8 @@
 define('CRON_PATH', dirname(__DIR__));
 include( CRON_PATH . '/public/db.inc.php' );
 include( CRON_PATH . '/public/lib.inc.php' );
+
+$cert = CRON_PATH . '/ck.pem' ;
 /*
 include( '../public/db.inc.php' );
 include( '../public/lib.inc.php' );
@@ -11,19 +13,18 @@ $period = 60;
 $repeat = 15;
 
 for( $i=0; $i<$repeat; $i++ ){
-    main();
+    main( $cert );
     sleep( $period );
 }
 
-function main(){
+function main( $cert ){
 
-    $cert = '../ck.pem';
     $testdevice = '178893bc87866f0f44d5d7bb4c3e5b84c0c3fa31b45e7e397a683f820919d89e';
-    $testmessage = "message sent at " . time( 'H:i:s' );
+    $testmessage = "test message sent at " . time( 'H:i:s' );
     sendIosNotification( $testdevice, $testmessage, $cert, false );
 
 	$db = new database( getDbcredentials() );
-    $db->log( time( 'H:i:s' ) . " running clearPushQueue";
+    $db->log( time( 'H:i:s' ) . " running clearPushQueue" );
     $pushlist = $db->getPushList();
     $n = count( $pushlist );
     echo "\n";
