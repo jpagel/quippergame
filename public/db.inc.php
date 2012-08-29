4533725de41c4f3da203b0863844f024cc92d512
Javier Fuchs <gamextar@hotmail.com>
2012-08-29 16:56:23 +0100

    test2

--- public/db.inc.php
+++ public/db.inc.php
@@ -444,16 +444,15 @@ class database{
 */
 
 
-		// JAVIER ADDED CODE
+		// JAVIER ADDED CODE github
 		
 		$sql = "INSERT INTO gamesession (game_id, user_id) VALUES ($gameid, $to)";
         $this->pdo->exec( $sql );
 	
-	
-	       //create gamehistory entry
-        $sql = "INSERT INTO gamehistory (game_id, user_id, score) VALUES ($gameid, $to, 0)
-                        ON DUPLICATE KEY UPDATE time = NOW()";
-        $this->pdo->exec( $sql );
+	//create gamehistory entry
+        //$sql = "INSERT INTO gamehistory (game_id, user_id, score) VALUES ($gameid, $to, 0)
+        //              ON DUPLICATE KEY UPDATE time = NOW()";
+        // $this->pdo->exec( $sql );
 		
 		// JAVIER ADDED CODE
 	
